30dea71bafa468edc01370a603d011025fad3e2c
Magno Carvalho <magno.carvalho@dorotech.com.br>
2022-08-28 15:44:23 -0300

    feat: mongo conecto charge to mongosse orm

--- src/common/dtos/page.dto.ts
+++ src/common/dtos/page.dto.ts
@@ -3,27 +3,27 @@ import { IsArray } from 'class-validator';
 import { PageOptionsDto } from './page-options.dto';
 
 export class PageDto<T> {
-  @IsArray()
-  @ApiProperty({ isArray: true })
-  readonly results: T[];
-
-  @ApiProperty()
+  @ApiProperty({ description: 'The page of results' })
   readonly page: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The number of results per page' })
   readonly limit: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The total number of results' })
   readonly totalDocs: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The total results of the all page' })
   readonly totalPages: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The hasNext of the page' })
   readonly hasNext: boolean;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The hasPrev of the page' })
   readonly hasPrev: boolean;
+
+  @IsArray()
+  @ApiProperty({ isArray: true, description: 'The results of the page' })
+  readonly results: T[];
 
   constructor(results: T[], pageOptionsDto: PageOptionsDto, totalDocs: number) {
     this.results = results;
